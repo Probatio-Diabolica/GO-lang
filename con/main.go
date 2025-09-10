0eb04b501b91269a985efd0c470477539511898e
Hearn <ayushkaintura1123@gmail.com>
2025-09-11 00:28:38 +0530

    routines

--- con/main.go
+++ con/main.go
@@ -1,48 +1,30 @@
 package main
 
 import (
-	"sync"
 	"fmt"
+	"sync"
 )
 
-
-func infite(wg *sync.WaitGroup){
+func infite(wg *sync.WaitGroup) {
 	defer wg.Done()
-	i:= 0
-	for j:= 0 ; j < 100 ; j++{
+	for j := 0; j < 5; j++ {
 		fmt.Println("{%v} A", j)
-		//i = j
-		j=i
 	}
 }
 
-func infi(wg *sync.WaitGroup){
+func infi(wg *sync.WaitGroup) {
 	defer wg.Done()
-	i:= 0
-	for j:= 0 ; j < 100 ; j++{
+	for j := 0; j < 5; j++ {
 		fmt.Println("{%v} B", j)
-		//i = j
-		j=i
 	}
 }
 
-func main(){
+func main() {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	
 	go infite(&wg)
 	go infi(&wg)
 
-
-	//go infite()
-	//go infi()
-	//infi()
-
-//	i:= 0
-//	for j:= range 10{
-	//	fmt.Println("{%d} a", j)
-//		j=i
-//	}
 	wg.Wait()
 }
